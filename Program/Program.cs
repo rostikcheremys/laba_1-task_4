cb6e182da50edbe732a275921de9c2c0f2906dc4
rostikcheremys <rostikcheremys@gmail.com>
2024-02-14 22:00:24 +0200

    Initial commit

---
+++ Program/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Program
+{
+    internal abstract class Program
+    {
+        private delegate double Calculate(double number);
+        
+        public static void Main()
+        {
+            while (true)
+            {
+                try
+                {
+                    string[] input = Console.ReadLine()?.Trim().Split();
+
+                    int command = Convert.ToInt32(input[0]);
+                    double number = Convert.ToDouble(input[1]);
+                    
+                    Calculate[] calculations = {
+                        x => Math.Sqrt(Math.Abs(number)),         
+                        x => Math.Pow(number, 3),         
+                        x => number - 1.25
+                    };
+                    
+                    Console.WriteLine($"Result: {calculations[command](number)}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error: {e}");
+                    break;
+                }
+            }
+        }
+    }
+}
